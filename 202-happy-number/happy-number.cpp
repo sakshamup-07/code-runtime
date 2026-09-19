1085565e8e71ed46be08f343bf771761731cc703
Saksham Upadhyay <154794993+sakshamup-07@users.noreply.github.com>
2026-09-19 06:09:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.42%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int sum = 0;
+        set <int> st;
+        while (n > 0) {
+            int ld = n % 10;
+            n = n / 10;
+            sum += ld * ld;
+            if (n == 0) {
+                if (sum == 1)
+                    return true;
+                if (st.count(sum))
+                    return false;
+                else {
+                    st.insert(sum);
+                    n = sum;
+                    sum = 0;
+                }
+            }
+        }
+        return false;
+    }
+};
