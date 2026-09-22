10c6ee61be3ca007ddff577e704020c28d5ca974
Saksham Upadhyay <154794993+sakshamup-07@users.noreply.github.com>
2026-09-22 23:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (16.01%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        char largest='-1';
-       for(auto it : n)
-       {
-        largest=max(it,largest);
-       }
-       return largest-'0';
+        return *max_element(n.begin() , n.end())-'0';
     }
 
 };
